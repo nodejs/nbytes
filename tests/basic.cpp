53067bcea002d287231aa5a4620c05d9b21ccee5
Yagiz Nizipli <yagiz@cloudflare.com>
2024-08-08 14:01:07 -0400

    update tests

--- tests/basic.cpp
+++ tests/basic.cpp
@@ -5,9 +5,9 @@
 #include <gtest/gtest.h>
 
 TEST(basic, swap_bytes16) {
-  std::vector<char> input = {1, 2, 3, 4, 5, 6, 7, 8};
+  std::vector<char> input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
   nbytes::SwapBytes16(input.data(), input.size());
-  std::vector<char> expected = {2, 1, 4, 3, 6, 5, 8, 7};
+  std::vector<char> expected = {2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15};
   EXPECT_EQ(input, expected);
   SUCCEED();
 }
